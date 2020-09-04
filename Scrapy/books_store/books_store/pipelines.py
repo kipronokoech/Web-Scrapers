ddd1cd0e5f750aecaa80ceea4c33a6120bfe2237
Kiprono Elijah Koech <kiprono@aims.ac.za>
2020-09-04 07:21:21 +0200

    Stored data on mySQL

--- Scrapy/books_store/books_store/pipelines.py
+++ Scrapy/books_store/books_store/pipelines.py
@@ -6,7 +6,8 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 # scrape -> item containers -> JSON/CSV/XML
 # scrape ->item containers -> Pipeline -> DB
-import sqlite3
+# import sqlite3
+import mysql.connector
 
 class BooksStorePipeline(object):
 
@@ -15,7 +16,14 @@ class BooksStorePipeline(object):
 		self.create_table()
 
 	def establish_connection(self):
-		self.connection = sqlite3.connect("books.db")
+		# self.connection = sqlite3.connect("books.db")
+		self.connection = mysql.connector.connect(
+			host = "127.0.0.1",
+			port = "3306",
+			user = "root",
+			password = "destinyx2719*KE",
+			database = "books_store",
+			auth_plugin='mysql_native_password')
 		self.cursor = self.connection.cursor()
 
 	def create_table(self):
@@ -30,7 +38,9 @@ class BooksStorePipeline(object):
 		self.store_data(item)
 		return item
 	def store_data(self,item):
-		self.cursor.execute("""INSERT INTO books_table VALUES(?,?,?)""",\
+		# self.cursor.execute("""INSERT INTO books_table VALUES(?,?,?)""",\
+		# 	(item["titles"],item["prices"],item["availability"]))
+		self.cursor.execute("""INSERT INTO books_table VALUES(%s,%s,%s)""",\
 			(item["titles"],item["prices"],item["availability"]))
 		self.connection.commit()
 

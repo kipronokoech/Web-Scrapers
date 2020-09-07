2839106dd7bfa55132d4c3d81e1438445355b1ae
Kiprono Elijah Koech <kiprono@aims.ac.za>
2020-09-07 19:48:23 +0200

    Added comments to keyword search script

--- Scrapy/AMZcockpit/AMZcockpit/spiders/keyword_search.py
+++ Scrapy/AMZcockpit/AMZcockpit/spiders/keyword_search.py
@@ -7,12 +7,12 @@ class KeywordSearchSpider(scrapy.Spider):
     name = 'keyword'
     allowed_domains = ['amazon.co.uk']
     page_number = 1
-    query = "https://www.amazon.co.uk/s?k={}&ref=nb_sb_noss"
     seo_overall = 0
     ppc_overall = 0
     def __init__(self,keyword,asin,**kwargs):
         super().__init__(**kwargs)
         self.keyword = keyword
+        self.query = "https://www.amazon.co.uk/s?k={}&ref=nb_sb_noss"
         self.found = False
         self.asin = asin
         self.search_url = self.query.format(self.keyword)
@@ -21,13 +21,18 @@ class KeywordSearchSpider(scrapy.Spider):
         print(100*"#")
         print(self.search_url)
         print(100*"#")
+    # Send a request to the market place url eg https://www.amazon.co.uk/
+    # send the result to the parse_index function
     def start_requests(self):
         yield Request(self.market_place, callback=self.parse_index)
-
+    # Send request to market place to conduct a search of keyword
+    # eg https://www.amazon.co.uk/s?k=laptop&ref=nb_sb_noss for laptop keyword
+    # send the result into the parse function below
     def parse_index(self, response):
         yield Request(self.search_url)
-
+    # The following function gets the page source for the search results
     def parse(self, response):
+    	# initialize item varible in the AmzcockpitItem items container
         item = AmzcockpitItem()
         # natural link counter
         seo = 0
@@ -41,16 +46,11 @@ class KeywordSearchSpider(scrapy.Spider):
             if not asin_:
                 continue
             print(asin_,self.asin,asin_ == self.asin)
-            # print(rank,asin_,asin_==self.asin)
-            # print(rank,asin_,self.asin, asin_==self.asin)
             if link is not None:
                 if link.startswith('/gp/slredirect'):
                     self.sponsored = True
                     ppc = ppc + 1 + rank_offset
-                    # item["rank"] = ppc
-                    # item["page_rank"] = KeywordSearchSpider.page_number
                     KeywordSearchSpider.ppc_overall = KeywordSearchSpider.ppc_overall + 1 + rank_offset
-                    # print(asin_,self.asin,"Sponsored",ppc)
                     if asin_==self.asin:
                         self.found = True
                         item["keyword"] = self.keyword
@@ -85,9 +85,10 @@ class KeywordSearchSpider(scrapy.Spider):
             	continue
         # Set the next page
         KeywordSearchSpider.page_number = KeywordSearchSpider.page_number + 1
-        next_page = "https://www.amazon.co.uk/s?k=Hair+dryer+holder&page={}&qid=1599413895&ref=sr_pg_{}"\
-        .format(KeywordSearchSpider.page_number,KeywordSearchSpider.page_number)
-        # Search the first 9 pages provided the item has not been found
+        next_page = "https://www.amazon.co.uk/s?k={}&page={}&qid=1599413895&ref=sr_pg_{}"\
+        .format(self.keyword,KeywordSearchSpider.page_number,KeywordSearchSpider.page_number)
+        # Search the first 60 pages provided the item has not been found.
+        # Stop making requests to the website once the item is found.
         if KeywordSearchSpider.page_number<60 and self.found == False:
             print(100*"#")
             print(KeywordSearchSpider.page_number)
